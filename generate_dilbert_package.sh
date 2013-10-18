ab7450c6f6f6de2023c96e41cca9fbc5faa7c75b
pelgrim <guskald@gmail.com>
2013-10-18 11:27:45 -0300

    Working package generation.

--- generate_dilbert_package.sh
+++ generate_dilbert_package.sh
@@ -3,6 +3,7 @@
 sudo -s "
   mkdir dilbert_package/usr/
   cp -r bin lib dilbert_package/usr/
+  cp Gemfile dilbert_package/usr/lib/dilbert-tickets/
   dpkg --build dilbert_package dilbert-tickets.deb
   rm -r dilbert_package/usr/*
 "
